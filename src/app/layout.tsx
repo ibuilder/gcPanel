2446771b1963acd2e2074df78e4d6af46c92903c
Matthew Emma (Matt) <iphoenix@gmail.com>
2025-04-26 00:11:46 +0000

    make it default in dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
       <ThemeProvider
         attribute="class"
-        defaultTheme="system"
+        defaultTheme="dark"
         enableSystem
         disableTransitionOnChange
       >
@@ -38,3 +38,4 @@ export default function RootLayout({
     </html>
   );
 }
+
